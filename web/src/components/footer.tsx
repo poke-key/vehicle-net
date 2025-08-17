d71deb9cc6af2d684e60031502b6fecbe8c2234a
Kunal Shrivastav <54164834+poke-key@users.noreply.github.com>
2025-08-17 12:36:52 -0700

    centered footer

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -6,7 +6,7 @@ import { Car, Github, Twitter } from "lucide-react";
 export function Footer() {
   return (
     <footer className="border-t bg-muted/40">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Car className="h-6 w-6" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
